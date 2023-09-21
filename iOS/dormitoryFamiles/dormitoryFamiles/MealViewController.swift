fd6001f3497b783410c15d8caf73aa5bb3ecdac0
Haena <ghkwlsspdl@naver.com>
2023-09-21 22:40:42 +0900

    [#2] FEAT: 데이터 크롤링 및 파싱 - 기숙사 식단을 홈페이지에서 크롤링하여 아침/점심/저녁 식단을 레이블 텍스트로 화면에 띄우기 - 스페이스처리를 개행으로 바꾸어 가독성 증진

--- iOS/dormitoryFamiles/dormitoryFamiles/MealViewController.swift
+++ iOS/dormitoryFamiles/dormitoryFamiles/MealViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftSoup
 
 enum Time: String {
     case morning = "morning"
@@ -20,6 +21,43 @@ class MealViewController: UIViewController {
         label.text = "오늘의 긱식 (\(formattedCurrentDate(year: false)))"
         return label
     }()
+    
+    
+    private lazy var todayMorning: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        spacingToNewline(time: .morning) { result in
+            label.text = result
+        }
+        return label
+    }()
+    
+    private lazy var todayLunch: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        spacingToNewline(time: .lunch) { result in
+            label.text = result
+        }
+        return label
+    }()
+    
+    private lazy var todayEvening: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        spacingToNewline(time: .evening) { result in
+            label.text = result
+        }
+        return label
+    }()
+    
+    private lazy var menuStackView: UIStackView = {
+        let st = UIStackView()
+        st.addArrangedSubview(todayMorning)
+        st.addArrangedSubview(todayLunch)
+        st.addArrangedSubview(todayEvening)
+        return st
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +79,7 @@ class MealViewController: UIViewController {
     }
     
     private func setupConstraints() {
-        [dateText].forEach {
+        [dateText, menuStackView].forEach {
             view.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -51,8 +89,71 @@ class MealViewController: UIViewController {
             dateText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             dateText.widthAnchor.constraint(equalToConstant: 500),
             dateText.heightAnchor.constraint(equalToConstant: 50),
+            
+            menuStackView.topAnchor.constraint(equalTo: dateText.bottomAnchor, constant: 15),
+            menuStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            menuStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            menuStackView.heightAnchor.constraint(equalToConstant: 500),
         ])
     }
+    
+    private func spacingToNewline(time: Time, completion: @escaping (String) -> Void) {
+        doCrawling(time: time) { result in
+            if let result = result {
+                let components = result.components(separatedBy: " ")
+                var modifiedResult = ""
+                
+                for (index, component) in components.enumerated() {
+                    if !(component == "Kcal" || component == "g") && (index > 0 && !components[index - 1].hasSuffix(" ")) {
+                        modifiedResult += "\n" + component
+                    } else if index == 0 {
+                        modifiedResult += "" + component
+                    } else {
+                        modifiedResult += " " + component
+                    }
+                }
+                completion(modifiedResult)
+            } else {
+                print("Crawling failed.")
+                completion("")
+            }
+        }
+    }
+
+    
+    private func doCrawling(time: Time, completion: @escaping (String?) -> Void) {
+        Task {
+            do {
+                let result = try await crawling(time: time)
+                completion(result)
+            } catch {
+                print("Error: \(error)")
+                completion(nil)
+            }
+        }
+    }
+    
+    private func crawling(time: Time) async throws -> String {
+        do {
+            let url = URL(string: "https://dorm.chungbuk.ac.kr/home/sub.php?menukey=20041")!
+            let html = try await String(contentsOf: url)
+            let document = try SwiftSoup.parse(html)
+            let trElement = try document.select("tr#\(formattedCurrentDate(year: true))").first()
+
+            if let trElement = trElement {
+                let morningTdElements = try trElement.select("td.\(time.rawValue)")
+                for morningTd in morningTdElements {
+                    return try morningTd.text()
+                }
+            } else {
+                print("TR Element not found.")
+            }
+        } catch {
+            print("Error: \(error)")
+        }
+        return ""
+    }
+
     
     
 

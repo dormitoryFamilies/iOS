1bf3d3db20e8db247211733b2d42920b53898e46
Haena <ghkwlsspdl@naver.com>
2023-09-21 22:21:30 +0900

    [#2] FEAT: 당일 날짜 레이블 세팅 - 오늘의 긱식 다음 날짜 데이터 세팅 - 날짜를 반환할 메서드 세팅 - 컨스트레인트 세팅

--- iOS/dormitoryFamiles/dormitoryFamiles/MealViewController.swift
+++ iOS/dormitoryFamiles/dormitoryFamiles/MealViewController.swift
@@ -9,9 +9,43 @@ import UIKit
 
 class MealViewController: UIViewController {
 
+    private lazy var dateText: UILabel = {
+        let label = UILabel()
+        label.text = "오늘의 긱식 (\(formattedCurrentDate(year: false)))"
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setupConstraints()
+       
+    }
 
+    private func formattedCurrentDate(year: Bool) -> String {
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        if year {
+            dateFormatter.dateFormat = "YYYY-MM-dd"
+        }else {
+            dateFormatter.dateFormat = "MM-dd"
+        }
+        return dateFormatter.string(from: currentDate)
+        
+    }
+    
+    private func setupConstraints() {
+        [dateText].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            dateText.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            dateText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            dateText.widthAnchor.constraint(equalToConstant: 500),
+            dateText.heightAnchor.constraint(equalToConstant: 50),
+        ])
     }
     
 
